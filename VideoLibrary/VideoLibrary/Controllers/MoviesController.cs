df27c839c1f60041f5fa78ba5d931aeddf5bd4fb
Vishwanath s <vishwanath.tns@gmail.com>
2018-08-15 00:47:58 +0530

    Customers and Movie Controllers basic
    Customers
           Index and Details methods added
    Movies
           Index methods added

---
+++ VideoLibrary/VideoLibrary/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoLibrary.Models;
+
+namespace VideoLibrary.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetAllMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetAllMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Bahubali-1"},
+                new Movie { Id = 1, Name = "Bahubali-2"}
+            };
+        }
+    }
+}
