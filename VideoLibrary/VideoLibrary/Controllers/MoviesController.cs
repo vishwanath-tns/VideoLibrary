013b109ee58aadc06a4c661fe2475085be48e1f3
Vishwanath s <vishwanath.tns@gmail.com>
2018-08-17 00:57:51 +0530

    Validations on Movie Form, Antiforgery, Custom Validations

--- VideoLibrary/VideoLibrary/Controllers/MoviesController.cs
+++ VideoLibrary/VideoLibrary/Controllers/MoviesController.cs
@@ -63,17 +63,26 @@ namespace VideoLibrary.Controllers
             var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
             if (movie == null)
                 return HttpNotFound();
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
             return View("MovieForm", viewModel);
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
